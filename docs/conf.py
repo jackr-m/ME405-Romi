489fe0d486ac62e28291203c30fb58ac2fc907ff
1stS1d <jack7nugget@gmail.com>
2023-12-11 14:19:01 -0800

    Added RTD theme to Sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,12 @@ author = 'Jack Miller & Casey Pickett'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+
+# Add napoleon (numpy & google docstring support) to the extensions list
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +28,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
