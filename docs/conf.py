c27528756f5c130d3001df0f3372423bec1ace55
1stS1d <jack7nugget@gmail.com>
2023-12-11 15:03:05 -0800

    Updated conf.py to make automodule work

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../'))
+
 project = 'ME405-Romi'
 copyright = '2023, Jack Miller & Casey Pickett'
 author = 'Jack Miller & Casey Pickett'
