da50d8d20ef617feb6c78ced18b17d2843f313ea
1stS1d <jack7nugget@gmail.com>
2023-12-15 17:39:32 -0800

    Fixed html_static_path error

--- docs/conf.py
+++ docs/conf.py
@@ -56,4 +56,4 @@ autodoc_mock_imports = ["pyb", "micropython"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['html/_static']
+html_static_path = []
